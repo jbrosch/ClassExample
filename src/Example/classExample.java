c9650114783635f9affa37d9f6640cb5031da51c
jbrosch <jbrosch@udel.edu>
2015-02-24 19:43:02 -0500

    testing committing to github

---
+++ src/Example/classExample.java
@@ -0,0 +1,28 @@
+package Example;
+
+public class classExample {
+
+	
+	public static int thisMethod(int a, int b, int c){
+		return a + b + c;
+	}
+	
+	int a;
+	String b;
+	
+	
+	public classExample(int a, String b){
+		this.a=a;
+		this.b=b;
+	}
+	
+	
+	public static void main(String[] args) {
+		int a = thisMethod(1,2,3);
+		
+		this
+		
+		System.out.println(thisMethod(1,2,3));
+	}
+
+}
